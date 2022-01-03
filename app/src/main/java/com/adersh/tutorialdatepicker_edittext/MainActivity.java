1891fdd72925a0975d3ecfabfaed53b59a88476e
Adersh <adersh1996@gmail.com>
2022-01-03 17:04:57 +0530

    final commit

--- app/src/main/java/com/adersh/tutorialdatepicker_edittext/MainActivity.java
+++ app/src/main/java/com/adersh/tutorialdatepicker_edittext/MainActivity.java
@@ -2,13 +2,47 @@ package com.adersh.tutorialdatepicker_edittext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    EditText selectDate;
+Calendar calendar=Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectDate = findViewById(R.id.calenderEt);
+
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR,i);
+                calendar.set(Calendar.MONTH,i1);
+                calendar.set(Calendar.DAY_OF_MONTH,i2);
+                updateLabel();
+            }
+        };
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    public void updateLabel(){
+        String myDateFormat="MM/dd/yy";
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(myDateFormat, Locale.US);
+        selectDate.setText(simpleDateFormat.format(calendar.getTime()));
     }
 }
